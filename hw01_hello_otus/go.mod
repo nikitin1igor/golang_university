0e1733700a8f3e8ba2118b494fd393f1557d6d21
Igor Nikitin <nikitin.igor3898@gmail.com>
2021-06-15 17:02:46 +0300

    fixed Reverse usage

--- hw01_hello_otus/go.mod
+++ hw01_hello_otus/go.mod
@@ -1,3 +1,5 @@
 module github.com/nikitin1igor/golang_university/hw01_hello_otus
 
 go 1.16
+
+require golang.org/x/example v0.0.0-20210407023211-09c3a5e06b5d
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,16 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
 
 func main() {
-	fmt.Print(Reverse("Hello, OTUS!"))
-}
-
-// Reverse func taken from golang repo.
-func Reverse(s string) string {
-	r := []rune(s)
-	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
-		r[i], r[j] = r[j], r[i]
-	}
-	return string(r)
+	fmt.Print(stringutil.Reverse(("Hello, OTUS!")))
 }
